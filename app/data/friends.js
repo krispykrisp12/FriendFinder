dc33177685008f7927cae823b1c46fbf32240d68
Kris Wilson <krispykrisp12@gmail.com>
2018-08-11 23:47:10 -0400

    Adding more changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,26 +6,32 @@ var friends = [
     },
     {
         "name": "Halle Berry",
-        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4CuQCqQdoniYWQOmXvIFqzUTU7DmRZKVZE_Xg5E-X-NkUS97zhttps://media",
-        "scores": [1,4,8,8,9,8,6,5,7,3]
+        "photo": "https://www.biography.com/.image/t_share/MTE1ODA0OTcxODYxNjQwNzE3/halle-berry-9542339-1-402.jpg",
+        "scores": [5,5,5,5,5,5,5,5,5,5]
     
     },
     {
         "name": "Denzel Washington",
-        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4CuQCqQdoniYWQOmXvIFqzUTU7DmRZKVZE_Xg5E-X-NkUS97zhttps://media",
-        "scores": [7,7,8,8,9,9,6,5,7,5]
+        "photo": "https://www.thetimes.co.uk/imageserver/image/methode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F18f8ec78-fb9f-11e7-9455-432f4ff19004.jpg?crop=2304%2C1296%2C354%2C24&resize=685",
+        "scores": [2,3,4,5,3,5,5,4,2,5]
     
     },
     {
         "name": "Lindsey Morgan",
-        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4CuQCqQdoniYWQOmXvIFqzUTU7DmRZKVZE_Xg5E-X-NkUS97zhttps://media",
-        "scores": [7,7,8,8,9,9,6,5,7,5]
+        "photo": "http://www.naludamagazine.com/wp-content/uploads/2015/05/Lindsey-Morgan.jpg",
+        "scores": [2,4,1,2,2,2,2,3,2,5]
     
     },
     {
         "name": "Johnny Depp",
-        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4CuQCqQdoniYWQOmXvIFqzUTU7DmRZKVZE_Xg5E-X-NkUS97zhttps://media",
-        "scores": [7,7,8,8,9,9,6,5,7,5]
+        "photo": "http://assets2.ignimgs.com/2014/08/20/johnny-depp-the-touristjpg-5f1620_1280w.jpg",
+        "scores": [1,4,5,2,5,3,3,2,2,5]
+    
+    },
+    {
+        "name": "Alex Morgan",
+        "photo": "https://pbs.twimg.com/profile_images/862064090436571138/qcZTA2Kb_400x400.jpg",
+        "scores": [2,4,3,2,4,5,2,4,2,5]
     
     }
     
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,13 @@
+var path  = require("path");
 var friends = require("../data/friends.js");
 
 module.exports = function(app){
   //api table list 
-  app.get("/api/tables", function (req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friends);
   })
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/friends", function(req, res) {
      if (friends.length) {
       friends.push(req.body);
       res.json();
